09176de5f53e8cc7535e62d409ad586d8be577f1
geonwk95 <geonwk95@naver.com>
2024-01-15 01:34:53 +0900

    Changes

--- src/과제/팀과제2/Survice.java
+++ src/과제/팀과제2/Survice.java
@@ -1,9 +1,15 @@
 package 과제.팀과제2;
 
 public class Survice {
-    String _class;
-    int price;
-    String tb;
-    int month;
-    int nOfP;
+    String _class = "베이직";
+    int price = 10000;
+    String tb = "5G";
+    int month = 1;
+    int nOfP = 1;
+
+
+
 }
+
+
+
