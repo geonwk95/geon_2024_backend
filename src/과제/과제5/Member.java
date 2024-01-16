9a6100816368a3e91956f741c3be26ef54782742
geonwk95 <geonwk95@naver.com>
2024-01-16 20:39:28 +0900

    Changes

--- src/과제/과제5/Member.java
+++ src/과제/과제5/Member.java
@@ -2,5 +2,10 @@ package 과제.과제5;
 
 
 public class Member {
+    String id;
+    String pw;
+    String name;
+    String phone;
+    String age;
 
 }
--- src/과제/과제5/MemberSys.java
+++ src/과제/과제5/MemberSys.java
@@ -7,17 +7,59 @@ public class MemberSys {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+
+		Member[] member1 = new Member[10];
+		System.out.println(member1);
+
 		while(true) {
 
 			System.out.println("\n\n--------- 회원 시스템 ---------");
 			System.out.print("1.회원가입 2.로그인    선택> ");
 			int ch = sc.nextInt();
 
-			if( ch == 1 ) {
+			if( ch == 1 ) { // 회원가입
+				// 1.입력받기
+				System.out.print("아이디 : "); String id = sc.next();
+				System.out.print("비밀번호 : "); String pw = sc.next();
+				System.out.print("이름 : "); String name = sc.next();
+				System.out.print("전화번호 : "); String phone = sc.next();
+				System.out.print("나이 : "); String age = sc.next();
+
+				// 2.객체화
+				Member member = new Member();
+				member.id = id;
+				member.pw = pw;
+				member.name = name;
+				member.phone = phone;
+				member.age = age;
+				for (int i = 0 ; i < member1.length ; i++){
+					if (member1[i] == null) {
+						member1[i] = member;
+						System.out.println(member1[i].age);
+
+						break; // 회원가입 출력 한번
+					}
+				}
+				System.out.println("회원가입 성공");
+
+
+
 
 			}
 			else if( ch == 2 ) {
+				System.out.print("아이디 : "); String id = sc.next();
+				System.out.print("비밀번호 : "); String pw = sc.next();
+
+				for (int i = 0 ; i < member1.length ; i++){
+					if( member1[i] != null){
+						if (member1[i].id.equals(id) && member1[i].pw.equals(pw) ){
+							System.out.println("로그인 성공");
+						}else {
+							System.out.println("로그인 실패");
+						}
+					}
+
+				}
 
 			}
 
